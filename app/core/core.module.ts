aa2b9a2e7c7c285724f8b2c78fc6c682d1cbdabf
zhiqiang.wu <zhiqiang.wu@chukou1.com>
2017-02-16 19:31:57 +0800

    init customers view

--- app/core/core.module.ts
+++ app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpModule, XSRFStrategy, CookieXSRFStrategy } from '@angular/http';
 
+import { DataService } from './data.service';
 import { EnsureModuleLoadedOnceGuard } from '../shared/ensureModuleLoadedOnceGuard';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { EnsureModuleLoadedOnceGuard } from '../shared/ensureModuleLoadedOnceGua
   providers: [
     //Default XSRF provider setup (change cookie or header name if needed): 
     //{ provide: XSRFStrategy, useValue: new CookieXSRFStrategy('XSRF-TOKEN', 'X-XSRF-TOKEN') },
+    DataService
   ] // these should be singleton
+
 })
 
 export class CoreModule extends EnsureModuleLoadedOnceGuard {
---
+++ app/customers/customers-grid.component.ts
@@ -0,0 +1,5 @@
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+import { ICustomer } from '../shared/interfaces';
+import { Sorter } from '../core/sorter';
+import { TrackByService } from '../core/trackby.service';
